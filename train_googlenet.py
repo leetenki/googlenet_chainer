74ad26deab8afe34e9e75292bc734f041c9d4452
tianqi-li <tianqi.li@dena.com>
2016-11-15 11:56:35 +0900

    add more detail to train_googlenet.py

--- train_googlenet.py
+++ train_googlenet.py
@@ -4,7 +4,17 @@ from chainer import optimizers, serializers, cuda, Variable
 import chainer.links as L
 from tuned_googlenet import GoogLeNet
 
+# load googlenet
 googlenet = GoogLeNet()
 serializers.load_npz('tuned_googlenet.model', googlenet)
 
+# optimizer
+optimizer = optimizers.Adam()
+optimizer.use_cleargrads()
+optimizer.setup(googlenet)
+
 # forward backward update...
+# y, loss, accuracy = googlenet(x, t)
+# optimizer.zero_grads()
+# loss.backward()
+# optimizer.update()
